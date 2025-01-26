bbdf5421897b9d2423635cd20344fe1b5ca69631
Dhiraj Nishad <70431995+Dhiraj-Nishad@users.noreply.github.com>
2025-01-26 20:47:21 +0530

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -7,11 +7,8 @@ API_ID = 29199461
 API_HASH = '5d5c0797293505649aaa30aa8d1af14a'
 SESSION_NAME = 'auto_buy_session'
 
-# Twitter credentials
-TWITTER_API_KEY = 'LimdXnhuXXgGC4IkXwMQ49Vt6'
-TWITTER_API_SECRET = 'wVJ8LSa3UiyhtROtlHpciFSdg8BnOM3rVeZG5G5EKLwTcXTUFt'
-TWITTER_ACCESS_TOKEN = '1878805655211159552-f9RRNgOf9wXWT4pdjEjr57rFwN2DXt'
-TWITTER_ACCESS_TOKEN_SECRET = 'C3ReRKNxZjsAI9HghxyyiMWpYLp9dW5wtP8Jl48hnQmab'
+# Twitter Bearer Token
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAEn9yQEAAAAAm3pKLa8OMfEqde9bSRbH3GUb0no%3Dx6HtObphKcph3iLT93gpJP6B4YSM9js6WKjSn6LO1a6xnaC8Xf'
 
 # List of source group IDs
 SOURCE_GROUP_IDS = ['@buybotttttt', '@buybottttt']  # Add more groups as needed
@@ -24,16 +21,20 @@ class MyStream(tweepy.StreamingClient):
     def on_tweet(self, tweet):
         if tweet.author.username in TARGET_TWITTER_USERS:
             message_text = tweet.text
-            client.send_message(TROJAN_BOT_ID, message_text)
             print(f"Forwarded tweet to Telegram: {message_text}")
+            client.send_message(TROJAN_BOT_ID, message_text)
 
     def on_error(self, status_code):
         if status_code == 420:
             return False  # Disconnects the stream on rate limiting
 
 def start_stream():
-    my_stream = MyStream(TWITTER_API_KEY, TWITTER_API_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-    my_stream.add_rules(tweepy.StreamRule(" OR ".join(f"from:{user}" for user in TARGET_TWITTER_USERS)))
+    my_stream = MyStream(BEARER_TOKEN)
+    
+    # Create rules to filter tweets from specified users
+    for user in TARGET_TWITTER_USERS:
+        my_stream.add_rules(tweepy.StreamRule(f"from:{user}"))
+
     my_stream.filter()
 
 def main():
